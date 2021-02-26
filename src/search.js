fbf36ae75e30b20e6a8ee3042271ade05500ccc0
ahacad <1393345075@qq.com>
2021-02-26 11:13:35 +0800

    feat: init

---
+++ src/search.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./search.css";
+import logo from "./images/savanah.png";
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div className="search-text">
+        Search Text
+        <img src={logo} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Search />, document.getElementById("root"));
---
+++ webpack.dev.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    index: "./src/index.js",
+    search: "./src/search.js",
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name][chunkhash:8].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: "babel-loader",
+      },
+      {
+        test: /.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /.less$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /.(png|jpg|gif|jpeg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10240,
+            },
+          },
+        ],
+      },
+      {
+        test: /.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "img/[name][hash:8].ext",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+  },
+};
